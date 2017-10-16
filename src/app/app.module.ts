2c05854765c749b483f3f6e7bd4134a386960a2b
rozeus <rozeuss@gmail.com>
2017-10-16 23:37:37 +0200

    Add about, footer, nav, home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
-import { PlayerListComponent } from './components/player-list/player-list.component';
+import {PlayerListComponent} from './components/player-list/player-list.component';
 import {PlayerService} from './services/player/player.service';
-import { NavComponent } from './components/nav/nav.component';
-import { FooterComponent} from './components/footer/footer.component';
+import {NavComponent} from './components/nav/nav.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {RouterModule} from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerListComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
   ],
   providers: [PlayerService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
